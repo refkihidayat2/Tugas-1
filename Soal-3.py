0bbab685592cbd1b145e22d1fe3bf256c0e6bdfa
refkihidayat2 <refkihidayat29@gmail.com>
2021-03-18 01:22:50 +0700

    Tugas-1

--- Soal-3.py
+++ Soal-3.py
@@ -1,6 +1,12 @@
-a = input("Masukkan nilai ujian teori :")
-b = input("Masukkan nilai ujian praktek :")
+a = float(input("Masukkan nilai ujian teori :"))
+b = float(input("Masukkan nilai ujian praktek :"))
 
 
-if a > 70 and b > 70 :
+if (a >= 70 and b >= 70) :
     print("Selamat, anda lulus!")
+elif (a >= 70 and b < 70):
+    print("Anda harus mengulang ujian praktek!")
+elif (a < 70 and b >= 70) :
+    print("Anda harus mengulang ujian teori!")
+else:
+    print("Anda harus mengulang ujian teori dan ujian praktek")
